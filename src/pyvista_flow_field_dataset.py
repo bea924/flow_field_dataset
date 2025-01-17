7d93f577af7ac2c61f37ddec3a5ab94a0acd9332
Ole Petersen <peteole2707@gmail.com>
2025-01-17 19:52:08 +0100

    Add demo

--- src/pyvista_flow_field_dataset.py
+++ src/pyvista_flow_field_dataset.py
@@ -3,13 +3,13 @@ import pyvista as pv
 from typing import Iterable, Iterator, Literal
 import numpy as np
 
-VolumeFieldType = Literal["velocity", "pressure", "temperature"]
+VolumeFieldType = Literal["Velocity", "Pressure", "Temperature"]
 SurfaceFieldType = Literal["pressure", "temperature", "shear_stress", "heat_flux"]
 
 class PyvistaSample:
-    def __init__(self, data: pv.UnstructuredGrid):
+    def __init__(self, data: pv.MultiBlock):
         self.data = data
-        self.surface_data = data.extract_surface()
+        #self.surface_data = data[0][0].extract_surface()
         # TODO: Check if data is a valid flow field dataset, i.e., has the necessary point data
     
     def save(self, filename: str):
@@ -20,9 +20,9 @@ class PyvistaSample:
         self.data.save(filename)
         
     def plot_surface(self, field: SurfaceFieldType):
-        self.data.plot(scalars=field)
+        self.data.plot(field)
     def plot_volume(self, field: VolumeFieldType):
-        self.data.plot(scalars=field)
+        self.data[0][0].plot(field, opacity=0.5)
         
     def get_points(self)->np.ndarray:
         """
@@ -48,8 +48,6 @@ class PyvistaSample:
     @classmethod
     def from_file(cls, filename: str):
         data = pv.read(filename)
-        if not isinstance(data, pv.UnstructuredGrid):
-            data = data.cast_to_unstructured_grid()
         # TODO not sure it works like this for pv.polydata objects as well as strucutresGrid
         return cls(data)
         
@@ -57,7 +55,7 @@ class PyvistaSample:
 class PyvistaFlowFieldDataset:
     def __init__(self, data_dir: str):
         self.data_dir = os.path.abspath(data_dir)
-        possible_extensions = [".ply", ".vtp", ".stl", ".vtk", ".geo", ".obj", ".iv", ".vtu"]
+        possible_extensions = [".ply", ".vtp", ".stl", ".vtk", ".geo", ".obj", ".iv", ".vtu",".cgns"]
         self.files = [f for f in os.listdir(self.data_dir) if os.path.splitext(f)[-1] in possible_extensions]
         self._index = 0  # Track iteration state
         
