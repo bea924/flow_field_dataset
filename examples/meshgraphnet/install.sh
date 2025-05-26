f33fed43b4c737dbe2172bc68602bc5659ecec85
Ole Petersen <peteole2707@gmail.com>
2025-05-26 08:29:24 +0200

    update dependencies

--- examples/meshgraphnet/install.sh
+++ examples/meshgraphnet/install.sh
@@ -1,2 +1,7 @@
-UV_HTTP_TIMEOUT=180 uv pip install  dgl -f https://data.dgl.ai/wheels/torch-2.4/cu121/repo.html
+#! /bin/bash
+
+UV_HTTP_TIMEOUT=180 uv pip install  dgl -f "https://data.dgl.ai/wheels/torch-2.4/cu124/repo.html"
 uv pip install -r requirements.txt
+
+# # Install the dgl backend for pytorch
+# uv pip install dgl-cu121 -f "https://data.dgl.ai/wheels/torch-2.4/cu121/repo.html"
