8f92e736b1a5b389dd27ea0db0b235eb40bde029
bea <beatrice.picco@tum.de>
2025-06-06 11:32:32 +0200

    First sphynx setup

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Dataset Library for 3D Machine Learning'
+project = 'CoolData'
+description = 'Dataset Library for 3D Machine Learning'
 copyright = '2025, Eya Chouaib, Firas Drass, \u200bJana Huhne, Ole Petersen,\u200b Beatrice Picco, Daniel Schenk\u200b'
 author = 'Eya Chouaib, Firas Drass, \u200bJana Huhne, Ole Petersen,\u200b Beatrice Picco, Daniel Schenk\u200b'
 
@@ -23,5 +24,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'classic'
 html_static_path = ['_static']
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc'
+]
---
+++ docs/usage/installation.rst
@@ -0,0 +1,8 @@
+Installation
+----------------
+
+Run
+
+.. code-block:: bash
+   pip install cooldata
+If you want to use the DGL support, you also need to install the `DGL library <https://www.dgl.ai/>`_, as documented `here <https://www.dgl.ai/pages/start.html>`_.
